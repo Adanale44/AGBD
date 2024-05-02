043b836ffe65d82a48726dfddbeb59d5d49413fe
adanale <dbsyc1214@gmail.com>
2024-05-02 11:39:24 -0300

    Ejecicio 5 completo ejercicios peliculas

--- peliculas.sql
+++ peliculas.sql
@@ -16,3 +16,10 @@ SELECT c.city, p.country FROM country p
 join city c on p.country_id = c.country_id
 ORDER by country ASC
 
+------5
+SELECT count(*) as cant_city, p.country FROM country p
+join city c on p.country_id = c.country_id
+GROUP by p.country_id
+ORDER by cant_city DESC
+
+-------6
