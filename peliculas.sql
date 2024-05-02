74f87b0835ee195be503b84ca5d1d4a0bd137ef7
adanale <dbsyc1214@gmail.com>
2024-05-02 11:39:05 -0300

    Ejecicio 4 completo ejercicios peliculas

--- peliculas.sql
+++ peliculas.sql
@@ -6,4 +6,13 @@ order by title ASC
 SELECT DISTINCT rating from film
 ORDER by rating asc
 
----3
+----3
+SELECT rental_rate, title from film 
+where rating = "PG-13" or "PG"
+order by rental_rate DESC, TITLE ASC
+
+-----4
+SELECT c.city, p.country FROM country p
+join city c on p.country_id = c.country_id
+ORDER by country ASC
+
