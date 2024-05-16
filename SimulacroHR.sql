511d5985b2029daa90f45936fe29b4623fb42195
adanale <dbsyc1214@gmail.com>
2024-05-16 10:32:35 -0300

    termine el ejercicio 1 de la evaluacion

--- SimulacroHR.sql
+++ SimulacroHR.sql
@@ -1,26 +1,28 @@
---1
+-1
 SELECT DISTINCT d.department_name from employees e
 join departments d on d.department_id = e.department_id
 order by d.department_name asc
----2
+--2
 SELECT DISTINCT salary from employees 
 ORDER by salary desc
-----3
+---3
 SELECT DISTINCT job_title, max_salary, min_salary from jobs
 WHERE job_title like "%Manager"
 order by max_salary DESC, min_salary DESC
------4
+----4
 SELECT DISTINCT r.region_name as region , c.country_name as pais from regions r
 join countries c on c.region_id = r.region_id
 ORDER by region_name ASC
-------5
+-----5
 SELECT e.first_name, e.last_name, e.salary, d.department_name  from employees e
 join departments d on d.department_id = e.department_id
 where e.salary between 9000 and 17000
 ORDER by salary DESC
--------6
+------6
 SELECT count(*) as cant_region, r.region_name from regions r
 join countries c on c.region_id = r.region_id
 GROUP by r.region_id
 HAVING cant_region > 5
 ORDER by cant_region DESC 
+
+-------7
