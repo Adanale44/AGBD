ff63990191c204ef2fc0c94dbe90c36e1e2ec2c3
adanale <dbsyc1214@gmail.com>
2024-05-16 10:44:16 -0300

    termine el ejercicio 2 de la evaluacion

--- Evaluacion/Ejercicio1.sql
+++ Evaluacion/Ejercicio1.sql
@@ -1,2 +1,7 @@
+--1
 SELECT DISTINCT type from crime_scene_report
 ORDER by type ASC
+
+--2
+SELECT p.name, d.car_make, d.age, d.car_model, d.plate_number from person p 
+JOIN drivers_license d on p.license_id = d.id
