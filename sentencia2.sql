ecac9d20b5f60934887296e658b0fdff8e699555
andale <dbsyc1214@gmail.com>
2024-05-30 09:18:58 -0300

    Termine de insertar el artista

--- sentencia2.sql
+++ sentencia2.sql
@@ -5,3 +5,7 @@ SELECT name FROM artists
 ORDER BY ArtistId DESC
 
 --Insertar Album
+INSERT INTO albums(Title,ArtistId)
+VALUES ("give me money",276);
+SELECT Title FROM albums
+ORDER BY ArtistId DESC
