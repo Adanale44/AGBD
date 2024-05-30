4cf6ea276939fb154f2923f803a8e62070aabbe1
andale <dbsyc1214@gmail.com>
2024-05-30 10:32:23 -0300

    Termine de insertar el album y las 3 canciones

--- sentencia2.sql
+++ sentencia2.sql
@@ -9,3 +9,9 @@ INSERT INTO albums(Title,ArtistId)
 VALUES ("give me money",276);
 SELECT Title FROM albums
 ORDER BY ArtistId DESC
+
+--Insertar 3 canciones
+INSERT INTO tracks(name, MediaTypeId, Milliseconds, UnitPrice)
+VALUES ("give",3,365041,1.99), ("me",3,315041,1.99), ("money",3,325041, 1.99);
+SELECT name, MediaTypeId,Milliseconds,UnitPrice FROM tracks
+ORDER BY name DESC
