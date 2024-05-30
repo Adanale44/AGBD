0f7460908aa9c2947f051a156b74af92818b47f9
andale <dbsyc1214@gmail.com>
2024-05-30 11:36:54 -0300

    Termine de modificar y  eliminar las cosas que inserte

--- sentencia2.sql
+++ sentencia2.sql
@@ -15,3 +15,22 @@ INSERT INTO tracks(name, MediaTypeId, Milliseconds, UnitPrice)
 VALUES ("give",3,365041,1.99), ("me",3,315041,1.99), ("money",3,325041, 1.99);
 SELECT name, MediaTypeId,Milliseconds,UnitPrice FROM tracks
 ORDER BY name DESC
+
+--Update is null
+UPDATE tracks set Composer = "money", AlbumId = 349
+where AlbumId is NULL
+
+UPDATE TRACKS 
+SET Bytes = 100000, GenreId = 1 
+WHERE AlbumId = 349;
+
+--Delete canciones
+DELETE FROM tracks WHERE name = "give" OR name = "me" or name = "money"
+DELETE FROM tracks WHERE tracksid = x
+--Delete album
+DELETE FROM albums where Title = "give me money";
+DELETE FROM albums where AlbumId = 349;
+
+--Delete artista
+DELETE FROM artists where ArtistId = 276
+DELETE FROM artists where name = OGLock
